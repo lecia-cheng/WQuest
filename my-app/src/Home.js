735936c2cf7393cb2d7fc7fbdfe90d5afe1bd0d1
lyriao <96245364+lyriao@users.noreply.github.com>
2024-02-06 16:30:15 -0500

    j

--- my-app/src/Home.js
+++ my-app/src/Home.js
@@ -3,11 +3,12 @@ import React, { useEffect } from "react";
 // navigate to different end points.
 import { Link } from "react-router-dom";
 import { useNavigate } from 'react-router-dom';
+import logo from './Assets/logo.png'
 
 const Home = () => {
     return (    
     <div className="App">
-        <div>Home</div>
+        <img className="logo" src={logo}></img>
     </div>
     );
 };
