4947c4d4d0e99af922bc346a97bb7958d4551b10
lyriao <96245364+lyriao@users.noreply.github.com>
2024-02-06 15:40:00 -0500

    route

---
+++ my-app/src/Home.js
@@ -0,0 +1,15 @@
+import React, { useEffect } from "react";
+// Importing Link from react-router-dom to 
+// navigate to different end points.
+import { Link } from "react-router-dom";
+import { useNavigate } from 'react-router-dom';
+
+const Home = () => {
+    return (    
+    <div className="App">
+        <div>Home</div>
+    </div>
+    );
+};
+ 
+export default Home;
---
+++ my-app/src/Leaderboard.js
@@ -0,0 +1,15 @@
+import React, { useEffect } from "react";
+// Importing Link from react-router-dom to 
+// navigate to different end points.
+import { Link } from "react-router-dom";
+import { useNavigate } from 'react-router-dom';
+
+const LeaderBoard = () => {
+    return (    
+    <div className="App">
+        <div>LeaderBoard</div>
+    </div>
+    );
+};
+ 
+export default LeaderBoard;
