54a3dbb3f721764345975884715bcf065f948683
Stephvenn <stephenshin792@hotmail.ca>
2024-02-06 16:50:56 -0500

    ethan changes

--- my-app/src/ProblemSets/ProblemSets.js
+++ my-app/src/ProblemSets/ProblemSets.js
@@ -1,15 +1,15 @@
 import React, { useEffect } from "react";
-// Importing Link from react-router-dom to 
+// Importing Link from react-router-dom to
 // navigate to different end points.
 import { Link } from "react-router-dom";
-import { useNavigate } from 'react-router-dom';
+import { useNavigate } from "react-router-dom";
 
 const ProblemSets = () => {
-    return (    
-    <div className="App">
-        <div>ProblemSets</div>
-    </div>
-    );
+	return (
+		<div className="App">
+			<div>ProblemSets</div>
+		</div>
+	);
 };
- 
+
 export default ProblemSets;
--- my-app/tailwind.config.js
+++ my-app/tailwind.config.js
@@ -1,11 +1,118 @@
-/** @type {import('tailwindcss').Config} */
-module.exports = {
-    content: [
-        "./src/**/*.{js,jsx,ts,tsx}",
-      ],
-  theme: {
-    extend: {},
-  },
-  plugins: [],
-}
 
+const config = {
+  content: [
+    "./src/**/*.{js,jsx,ts,tsx}",
+  ],
+  theme: {
+    extend: {
+      colors: {
+        primary: "#282C34",
+        secondary: "#393738",
+        tertiary: "#4F7CAC",
+      },
+      transitionDelay: {
+        '0': '0ms',
+        '100': '100ms',
+        '200': '200ms',
+        '300': '300ms',
+        '400': '400ms',
+        '500': '500ms',
+        '1000': '1000ms',
+      },
+      screens: {
+        sm: "640px", // Small screens, like smartphones
+        md: "768px", // Medium screens, like tablets
+        lg: "1024px", // Large screens, like laptops
+        xl: "1280px", // Extra large screens, like desktops
+        "2xl": "1536px", // Extra extra large screens
+        // Add more custom screen sizes as needed
+      },
+    },
+
+    animation: {
+      slideup: "slideup 1s ease-in-out",
+      slidedown: "slidedown 1s ease-in-out",
+      slideleft: "slideleft 1s ease-in-out",
+      slideright: "slideright 1s ease-in-out",
+      wave: "wave 1.2s linear infinite",
+      slowfade: "slowfade 2.2s ease-in-out",
+      bounce: "bounce 1s infinite",
+      grow: "grow 1s ease-in forwards",
+      fadeInUp: 'fadeInUp 1s ease-out forwards',
+      fadeInOut: "fadeInOut 2s linear infinite",
+      minimize: 'minimizeEffect 0.5s ease-in-out forwards',
+      maximize: 'maximizeEffect 0.5s ease-in-out forwards',
+    },
+
+    keyframes: {
+      slideup: {
+        from: { opacity: "0", transform: "translateY(25%)" },
+        to: { opacity: "1", transform: "translateY(0)" },
+      },
+      slidedown: {
+        from: { opacity: "0", transform: "translateY(-25%)" },
+        to: { opacity: "1", transform: "translateY(0)" },
+      },
+      slideleft: {
+        from: { opacity: "0", transform: "translateX(-20px)" },
+        to: { opacity: "1", transform: "translateX(0)" },
+      },
+      slideright: {
+        from: { opacity: "0", transform: "translateX(20px)" },
+        to: { opacity: "1", transform: "translateX(0)" },
+      },
+      slowfade: {
+        from: { opacity: "0" },
+        to: { opacity: "1" },
+      },
+      bounce: {
+        '0%, 100%': { 
+          transform: 'translateY(-25%)',
+          'animation-timing-function': 'cubic-bezier(0.8, 0, 1, 1)', // Optional if you want to include it here
+        },
+        '50%': { 
+          transform: 'translateY(0)',
+          'animation-timing-function': 'cubic-bezier(0, 0, 0.2, 1)', // Optional if you want to include it here
+        },
+      },
+      grow: {
+        from: { transform: "scale(0.6)" },
+        to: { transform: "scale(1)" },
+      },
+      fadeInUp: {
+        '0%': { opacity: '0', transform: 'translateY(20px)' },
+        '100%': { opacity: '1', transform: 'translateY(0)' },
+      },
+      fadeInOut: {
+        '0%': { opacity: '1' },
+        '50%': { opacity: '0' },
+        '100%': { opacity: '1' },
+      },
+      minimizeEffect: {
+        '0%': {
+          transform: 'translateY(0) scale(1)',
+          opacity: '1',
+        },
+        '100%': {
+          transform: 'translateY(100px) scale(0.5)',
+          opacity: '0',
+        },
+      },
+      maximizeEffect: {
+        '0%': {
+          transform: 'translateY(100px) scale(0.5)',
+          opacity: '0',
+        },
+        '100%': {
+          transform: 'translateY(0) scale(1)',
+          opacity: '1',
+        },
+      },
+    },
+    plugins: [],
+  },
+};
+
+
+
+export default config;
